609ac9225cd655f9fc939c7f2ef85326accb5515
matt <mattpitner09@gmail.com>
2019-04-30 14:16:26 -0600

    adding react project

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+
+function App() {
+  return (
+
+    <Router>
+      <>
+
+        <Nav />
+
+        <Switch>
+
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+
+        </Switch>
+
+      </>
+
+    </Router>
+  );
+}
+
+export default App;
